b9dfe91ffcb82da80398d7bb75282ed8631b3c16
Sherry <YuHuijie@YudeMacBook-Air.local>
2018-09-19 17:11:34 +1000

    1.4 Security Scene completed

---
+++ Oceans11Project/Assets/Oceans11/Materials/Scenes/1.4 Security/PlayAudio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour {
+
+    public AudioSource au;
+    public AudioClip ac;
+    private void OnMouseEnter()
+    {
+        au.clip = ac;
+        au.Play();
+    }
+}
